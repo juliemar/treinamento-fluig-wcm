a0e373c1746ebadb127329caee4e684372fd1a95
juliemar.berri <juliemar.berri@gmail.com>
2016-04-19 15:26:19 -0300

    resultado final widget de favoritos

--- wcm/widget/wcm_treinamento_favorito/src/main/resources/view.ftl
+++ wcm/widget/wcm_treinamento_favorito/src/main/resources/view.ftl
@@ -1,7 +1,26 @@
 <div id="TreinamentoFavorito_${instanceId}" class="super-widget wcm-widget-class fluig-style-guide"
-     data-params="TreinamentoFavorito.instance({message: 'Hello world'})">
+	data-params="TreinamentoFavorito.instance({message: 'Hello world'})">
 
-    <DIV id="favorito_${instanceId}">Treinamento Favorito</DIV>
-    
+	<DIV id="favorito_${instanceId}"></DIV>
+
+
+	<script type="template" class="linha-customizada">
+		<tr>
+			<td>{{documentId}}</td>
+			<td>{{documentDescription}}</td>
+			<td>
+				<b>{{version}}</b>
+			</td>
+			<td>
+				<a target="_blank"
+					href="/portal/p/note/ecmnavigation?app_ecm_navigation_doc={{documentId}}">Visualizar</a>
+			</td>
+		</tr>
+	</script>
+
+	<script type="text/template" class="template_area_buttons">
+		<button class="btn btn-primary" data-your-action-1>Action 1</button>
+		<button class="btn btn-primary" data-your-action-2>Action 2</button>
+	</script>
 
 </div>
--- wcm/widget/wcm_treinamento_favorito/src/main/webapp/resources/js/wcm_treinamento_favorito.js
+++ wcm/widget/wcm_treinamento_favorito/src/main/webapp/resources/js/wcm_treinamento_favorito.js
@@ -1,27 +1,108 @@
 var TreinamentoFavorito = SuperWidget.extend({
 	message: null,
-
+	myTable:null,
+	
 	init: function () {
 		var that= this;
 		try{
-			$.getJSON( "/api/public/2.0/favorites/findFavoritesDocuments/juliemar.berri", 
-					function( data ) {
-				$.each(data.content, function( index, value ) {
+
+			this.mostraDatatableSozinho();	
+
+//			$.getJSON( "/api/public/2.0/favorites/findFavoritesDocuments/juliemar.berri", 
+//					function( data ) {
+//
+//				that.mostraDatatable(data.content);
+//			});	
 
 
-					$("#favorito_"+that.instanceId).append("<p>"+value.documentDescription+"</p>");
+//			$.getJSON( "/api/public/2.0/favorites/findFavoritesDocuments/juliemar.berri", 
+//			function( data ) {
+//			$.each(data.content, function( index, value ) {
 
-				});
-			});	
+
+//			$("#favorito_"+that.instanceId).append("<p>"+value.documentDescription+"</p>");
+
+//			});
+//			});	
 		}catch(e){
 			console.log(e);
 		}
 	},
 
+	
+	mostraDatatableSozinho:function(){
+
+		this.myTable = FLUIGC.datatable("#favorito_"+this.instanceId, {
+			dataRequest: {
+		        url: '/api/public/2.0/favorites/findFavoritesDocuments/juliemar.berri',
+		        options: {
+		            contentType:'application/json',
+		            dataType: 'json',
+		            crossDomain: true,
+		            cache: false
+		        },
+		        root: 'content'
+		    },
+			renderContent: ".linha-customizada",
+			 search: {
+				        enabled: false
+				    },
+				actions: {
+			        enabled: true,
+			        template: '.template_area_buttons',
+			        actionAreaStyle: 'col-md-9'
+			    },
+				header: [
+				         {'title': 'Código'},
+				         {'title': 'Nome'},
+				         {'title': 'Versão'},
+				         {'title': 'Visualizar'}
+				         ]
+		}, function(err, data) {
+			// DO SOMETHING (error or success)
+		});
+	},
+	
+	
+	mostraDatatable:function(data){
+
+		this.myTable = FLUIGC.datatable("#favorito_"+this.instanceId, {
+			dataRequest: data,
+			renderContent: ".linha-customizada",
+			 search: {
+				        enabled: false
+				    },
+				actions: {
+			        enabled: true,
+			        template: '.template_area_buttons',
+			        actionAreaStyle: 'col-md-9'
+			    },
+				header: [
+				         {'title': 'Código'},
+				         {'title': 'Nome'},
+				         {'title': 'Versão'},
+				         {'title': 'Visualizar'}
+				         ]
+		}, function(err, data) {
+			// DO SOMETHING (error or success)
+		});
+	},
+
+
 	bindings: {
 		local: {
-			'show-message': ['click_showMessage']
+			'show-message': ['click_showMessage'],
+			'your-action-1':['click_addLinha']
 		}
+	},
+	
+	addLinha:function(){
+		var data={
+				documentId:'333',
+				documentDescription:"Documento Fake",
+				version:"3000"
+		};
+		this.myTable.addRow(0, data);
 	},
 
 	showMessage: function () {
